bda583bb17ab3077151c2c549a602a0454ac50fc
aukkarapong <54120755+aukkarapong@users.noreply.github.com>
2020-09-24 16:56:27 +0700

    Update home.ts

--- checkbox/home.ts
+++ checkbox/home.ts
@@ -6,14 +6,21 @@ import { NavController } from "ionic-angular";
   templateUrl: "home.html",
 })
 export class HomePage {
-  drink: boolean = false;
-  smoke: boolean = false;
-  diseases: boolean = false;
+  drink: boolean=false
+  smoke: boolean=false
+  diseases: boolean=false
+  dk:any
   constructor(public navCtrl: NavController) {}
 
   handleClickSaveButton() {
-    console.log("drink = ", this.drink);
-    console.log("smoke = ", this.smoke);
-    console.log("diseases = ", this.diseases);
+    if (this.drink = true){
+      console.log("drinks is selected");
+    }
+    if (this.smoke = true){
+      console.log("smoke is selected");
+    }
+    if (this.diseases = true){
+      console.log("diseases is selected");
+    }
   }
 }
